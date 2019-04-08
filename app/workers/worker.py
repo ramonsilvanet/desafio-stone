b5aaf799285c906a86eb3e5d3eb1df8c89dbd47a
Ramon Silva <ramon.silva@m4u.com.br>
2019-04-08 19:15:02 -0300

    Melhorando o codigo de threads

--- app/workers/worker.py
+++ app/workers/worker.py
@@ -1,19 +1,23 @@
 import threading, time, uuid
+from threading import Thread
 
-class Worker:
+class Worker(Thread):
 
-    id
-
-    def __init__(self):
+    def __init__(self, args):
+        Thread.__init__(self)
+        
+        self._args = args
         self.id = uuid.uuid4()
 
-    def execute(self, args):
-        t1 = threading.Thread(target=self.__calculate(), args=(self, args))
-        t1.start()
-        t1.join()
 
-    def __calculate(self, n):
+    def run(self):
+        self.__calculate(self, self._args)
+    
+    def __calculate(n):
         if n < 2:
             return n
-        time.sleep(1) # wait 1 second
-        return fibo_worker(self, n-1) + fibo_worker(self, n-2) 
+        
+        return self.__calculate(self, n-1) + self.__calculate(self, n-2)
+
+
+    
