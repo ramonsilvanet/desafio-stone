35b04b3e9d4c7b30887e1c3e9bb51979ca02a7b6
Ramon Silva <ramon.silva@m4u.com.br>
2019-04-26 19:31:53 -0300

    [WIP] - Update status of running job

--- app/jobs/task.py
+++ app/jobs/task.py
@@ -1,21 +1,29 @@
 import time
 from app.celery import celery
 from app.jobs import bp
-from flask import jsonify
+from flask import jsonify, url_for
 
 @celery.task(bind=True)
-def long_job(seconds=100):
-    print("Starting task")
+def long_job(seconds=100):    
     for i in range(seconds):
-        print(i)
+
+        message = "{}/{}".format(i, seconds)
+
+        self.update_state(state='PROGRESS',
+            meta={'current': i, 'total': total,
+                                'status': message})
         time.sleep(1)
-    print("Task completed")
+
+    return {'current': seconds, 'total': seconds, 
+        'status': 'Task completed!',
+        'result': 'ok'}
 
 
 @bp.route("/job", methods=['POST'])
 def create_job():
     job = long_job.apply_async()
-    return jsonify({'job_id': job.id}), 201
+    return jsonify({'job_id': job.id, 
+        'url' : url_for("jobs.job_status", job_id=job.id)}), 201
 
 @bp.route("/job/<job_id>", methods=['GET'])
 def job_status(job_id):
